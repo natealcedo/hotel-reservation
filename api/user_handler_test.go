07f6303183136a8a9882dfcff36edd52f64dd56e
Nathaniel Alcedo <nate@natealcedo.dev>
2024-07-30 21:58:44 +0800

    Add test case for
    1. Create a user using HandlePostUser
    2. Ensure that we get the same user from the created user

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -129,3 +129,59 @@ func TestHandleGetUsers(t *testing.T) {
 		t.Errorf("expected empty array but got %d", len(*users))
 	}
 }
+
+func TestHandleGetUserById(t *testing.T) {
+	testingDb := setup(t)
+	defer testingDb.tearDown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(testingDb.UserStore)
+	app.Post("/", userHandler.HandlePostUser)
+	app.Get("/:id", userHandler.HandleGetUserById)
+
+	params := types.CreateUserParams{
+		FirstName: "Nate",
+		LastName:  "Alcedo",
+		Email:     "natealcedo@gmail.com",
+		Password:  "randompassword",
+	}
+
+	b, _ := json.Marshal(params)
+
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := &types.User{}
+
+	req = httptest.NewRequest("GET", "/"+user.ID.Hex(), nil)
+	_ = json.NewDecoder(resp.Body).Decode(&user)
+
+	if len(user.ID) == 0 {
+		t.Errorf("expected user id to be set")
+	}
+
+	if len(user.EncryptedPassword) > 0 {
+		t.Errorf("expected encrypted password not to be returned from endpoint")
+	}
+
+	if resp.StatusCode != fiber.StatusOK {
+		t.Errorf("expected %d but got %d", fiber.StatusOK, resp.StatusCode)
+	}
+
+	if user.FirstName != params.FirstName {
+		t.Errorf("expected %s but got %s", params.FirstName, user.FirstName)
+	}
+
+	if user.LastName != params.LastName {
+		t.Errorf("expected %s but got %s", params.LastName, user.LastName)
+	}
+
+	if user.Email != params.Email {
+		t.Errorf("expected %s but got %s", params.Email, user.Email)
+	}
+}
