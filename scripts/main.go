0851be34a6526f43318218bcfdde3ee1aac21720
Nathaniel Alcedo <nate@natealcedo.dev>
2024-07-31 14:43:34 +0800

    Add a working script for seeding rooms and hotels

--- scripts/main.go
+++ scripts/main.go
@@ -1,28 +1,48 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/natealcedo/hotel-reservation/db"
+	"github.com/natealcedo/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
 func main() {
-	//ctx := context.Background()
-	//client, err := mongo.Connect(ctx, options.Client().ApplyURI(db.DBURI))
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
-	//hotel := &types.Hotel{
-	//	Name:     "Hotel California",
-	//	Location: "California",
-	//}
-	////_ := &types.Room{
-	////	Type:      types.SingleRoomType,
-	////	BasePrice: 99.9,
-	////}
-	//
-	//insertedHotel, err := hotelStore.InsertHotel(ctx, hotel)
-	//
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//fmt.Println(insertedHotel)
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+	roomStore := db.NewMongoRoomStore(client, db.DBNAME)
+
+	hotel := &types.Hotel{
+		Name:     "Hotel California",
+		Location: "California",
+	}
+
+	insertedHotel, err := hotelStore.InsertHotel(ctx, hotel)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	room := &types.Room{
+		HotelID:   insertedHotel.ID,
+		Type:      types.SingleRoomType,
+		BasePrice: 99.9,
+	}
+
+	insertedRoom, err := roomStore.InsertRoom(ctx, room)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(insertedHotel)
+	fmt.Println(insertedRoom)
 
 }
