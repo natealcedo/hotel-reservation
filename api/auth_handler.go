9858cf974c23d2a669217c8f596a88c7631eff11
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-03 22:50:35 +0800

    Return user after auth

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -1,14 +1,13 @@
 package api
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/natealcedo/hotel-reservation/db"
 )
 
 type AuthParams struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" `
+	Password string `json:"password" `
 }
 
 type AuthHandler struct {
@@ -22,10 +21,21 @@ func NewAuthHandler(store *db.Store) *AuthHandler {
 }
 
 func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
-	var authParams *AuthParams
+	// When using body parser, the variable has to be a struct, not a pointer to a struct
+	// When passing the variable to c.BodyParser, it has to be a pointer to the variable
+	// Not doing so will result in a nil dereference error
+	var authParams AuthParams
 	if err := c.BodyParser(&authParams); err != nil {
 		return err
 	}
-	fmt.Println(authParams)
-	return nil
+
+	user, err := h.store.User.GetUserByEmail(c.Context(), authParams.Email)
+
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"error": "user not found",
+		})
+	}
+
+	return c.JSON(user)
 }
