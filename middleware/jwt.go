4f46f0c24894575c72e23910a9e3df11b74c7133
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-03 21:07:22 +0800

    Implement a basic jwt middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -3,15 +3,20 @@ package middleware
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"os"
 )
 
 func JWTAuthentication(c *fiber.Ctx) error {
 	token, ok := c.GetReqHeaders()["X-Api-Token"]
-	if !ok {
-		return fmt.Errorf("Anauthorized")
+	if !ok || len(token) == 0 {
+		return fmt.Errorf("unauthorized")
 	}
 
-	fmt.Println(token)
+	if err := parseToken(token[0]); err != nil {
+		fmt.Println("error parsing token", err)
+		return fmt.Errorf("unauthorized")
+	}
 
 	if err := c.Next(); err != nil {
 		return err
@@ -19,4 +24,31 @@ func JWTAuthentication(c *fiber.Ctx) error {
 
 	return nil
 
+}
+
+func parseToken(tokenStr string) error {
+	unauthorized := fmt.Errorf("unauthorized")
+	parsedToken, err := jwt.Parse(tokenStr, func(token *jwt.Token) (any, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Printf("unexpected signing method: %v\n", token.Header["alg"])
+			return nil, unauthorized
+		}
+
+		secret := os.Getenv("JWT_SECRET")
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return unauthorized
+	}
+
+	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
+		fmt.Println(claims)
+	} else {
+		fmt.Println(err)
+	}
+
+	return unauthorized
 }
