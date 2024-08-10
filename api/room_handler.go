077beb4b5f0f15307954c9f3322e11a0fa62d025
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-10 15:02:14 +0800

    Add a BookRoomParams struct
    Implement logic for creating a booking struct and print it out first

--- api/room_handler.go
+++ api/room_handler.go
@@ -1,14 +1,18 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/natealcedo/hotel-reservation/db"
+	"github.com/natealcedo/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
 type BookRoomParams struct {
-	FromDate time.Time `json:"fromDate"`
-	TillDate time.Time `json:"tillDate"`
+	FromDate   time.Time `json:"fromDate"`
+	TillDate   time.Time `json:"tillDate"`
+	NumPersons int       `json:"numPersons"`
 }
 
 type RoomHandler struct {
@@ -22,11 +26,33 @@ func NewRoomHandler(s *db.Store) *RoomHandler {
 }
 
 func (h *RoomHandler) HandleBookRoom(c *fiber.Ctx) error {
-	//roomOID, err := primitive.ObjectIDFromHex(c.Params("id"))
-	//
-	//if err != nil {
-	//	return err
-	//}
+	var params BookRoomParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	roomId, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	user, ok := c.Context().Value("user").(*types.User)
+
+	if !ok {
+		return c.Status(fiber.StatusInternalServerError).JSON(genericResponse{
+			Msg:  "internal server error",
+			Type: "error",
+		})
+	}
+
+	booking := types.Booking{
+		UserID:     user.ID,
+		RoomID:     roomId,
+		NumPersons: params.NumPersons,
+		FromDate:   params.FromDate,
+		TillDate:   params.TillDate,
+	}
+
+	fmt.Printf("%+v\n", booking)
 
 	return nil
 }
