e1307b429702c439b69b0563a739673f68110b2c
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-11 16:08:43 +0800

    Implement a get booking by id

--- db/booking_store.go
+++ db/booking_store.go
@@ -14,6 +14,7 @@ type BookingStore interface {
 
 	InsertBooking(context.Context, *types.Booking) (*types.Booking, error)
 	GetBookings(context.Context, bson.M) ([]*types.Booking, error)
+	GetBookingByID(context.Context, string) (*types.Booking, error)
 }
 
 type MongoBookingStore struct {
@@ -52,6 +53,20 @@ func (s *MongoBookingStore) GetBookings(ctx context.Context, filter bson.M) ([]*
 
 	return bookings, nil
 
+}
+
+func (s *MongoBookingStore) GetBookingByID(ctx context.Context, id string) (*types.Booking, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var booking types.Booking
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&booking); err != nil {
+		return nil, err
+	}
+
+	return &booking, nil
 }
 
 func (s *MongoBookingStore) Drop(ctx context.Context) error {
