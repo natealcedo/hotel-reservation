98877d96091684b5e9023bb7dcbc54fa499a7e87
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-11 16:24:35 +0800

    Update room struct to remove room size enum

--- types/room.go
+++ types/room.go
@@ -4,16 +4,12 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type RoomSize string
 
-const (
-	Small  RoomSize = "small"
-	Normal          = "normal"
-	King            = "king"
-)
-
 type Room struct {
-	ID      primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Size    RoomSize           `json:"size" bson:"size"`
-	Seaside bool               `json:"seaside" bson:"seaside"`
-	Price   float64            `json:"price" bson:"price"`
-	HotelID primitive.ObjectID `json:"hotelID" bson:"hotelID"`
+	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	// small, normal, kingsize
+	Size      string             `json:"size" bson:"size"`
+	Seaside   bool               `json:"seaside" bson:"seaside"`
+	Price     float64            `json:"price" bson:"price"`
+	HotelID   primitive.ObjectID `json:"hotelID" bson:"hotelID"`
+	Available bool               `json:"-" bson:"available"`
 }
