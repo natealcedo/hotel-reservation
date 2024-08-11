0e09d7159e8fdc035f893b471ff64736d24ae16b
Nathaniel Alcedo <nate@natealcedo.dev>
2024-08-11 16:30:09 +0800

    Remove Available field from Room struct

--- types/room.go
+++ types/room.go
@@ -7,9 +7,8 @@ type RoomSize string
 type Room struct {
 	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	// small, normal, kingsize
-	Size      string             `json:"size" bson:"size"`
-	Seaside   bool               `json:"seaside" bson:"seaside"`
-	Price     float64            `json:"price" bson:"price"`
-	HotelID   primitive.ObjectID `json:"hotelID" bson:"hotelID"`
-	Available bool               `json:"-" bson:"available"`
+	Size    string             `json:"size" bson:"size"`
+	Seaside bool               `json:"seaside" bson:"seaside"`
+	Price   float64            `json:"price" bson:"price"`
+	HotelID primitive.ObjectID `json:"hotelID" bson:"hotelID"`
 }
